8636fd56bd4063b6c776668dc9e903aa23fb49eb
Leonardo Gatica <lgatica@protonmail.com>
2018-04-30 11:50:10 -0300

    feat(app): add healthcheck to express

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 'use strict'
 
+const http = require('http')
+const mongoose = require('mongoose')
+const terminus = require('@godaddy/terminus')
 const app = require('./app')
 const { port } = require('./config')
 
-app.listen(port, () => {
-  console.log(`Listen in port ${port}`) // eslint-disable-line
-})
+const server = http.createServer(app)
+
+/**
+ * Metodo encargado ejecutar tareas previas a terminar la aplicación
+ */
+async function onSignal () {
+  await mongoose.connection.close()
+}
+
+/**
+ * Metodo encargado de verificar que la aplicación este viva
+ */
+async function onHealthCheck () {
+  return mongoose.connection.readyState === 1
+}
+
+const options = {
+  healthChecks: { '/healthcheck': onHealthCheck },
+  timeout: 1000,
+  signal: process.env.HEALTHCHECK_SIGNAL || 'SIGINT',
+  onSignal
+}
+terminus(server, options)
+
+server.listen(port)
+console.log(`Listen in port ${port}`) // eslint-disable-line

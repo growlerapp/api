aef37d57132a8ea75a5ecb777a2072070a454eb2
Leonardo Gatica <lgatica@protonmail.com>
2019-05-28 16:50:29 -0400

    fix(deps): migrate to @sentry/node

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 'use strict'
 
-const os = require('os')
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Raven = require('raven')
+const Sentry = require('@sentry/node')
 const helmet = require('helmet')
 const config = require('./config')
 const pkg = require('../package.json')
@@ -19,26 +18,25 @@ mongoose.set('useCreateIndex', true)
 
 const app = express()
 
-Raven.config(process.env.SENTRY_TOKEN, {
-  release: pkg.version,
-  environment: process.env.NODE_ENV || 'development',
-  server_name: process.env.HOSTNAME || os.hostname(),
-  captureUnhandledRejections: true,
-  autoBreadcrumbs: true
-}).install()
+Sentry.init({
+  dsn: process.env.SENTRY_TOKEN,
+  release: process.env.SENTRY_RELEASE || pkg.version,
+  serverName: process.env.SENTRY_NAME,
+  environment: process.env.SENTRY_ENVIRONMENT || config.env
+})
 
 // Mejorar seguridad
 app.use(helmet())
 app.disable('x-powered-by')
 
-app.use(Raven.requestHandler())
+app.use(Sentry.Handlers.requestHandler())
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
 router(app)
 
-app.use(Raven.errorHandler())
+app.use(Sentry.Handlers.errorHandler())
 app.use((err, req, res, next) => {
   console.error(err) // eslint-disable-line
   res.sendStatus(500)
